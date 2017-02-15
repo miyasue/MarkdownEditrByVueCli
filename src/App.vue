e48cf0fbb5c7d1beafde5da59f7076870c09b5bc
宮末 啓吾 <miyasue.keigo@fox.local>
2017-02-15 18:46:36 +0900

    update: style

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
-    <textarea placeholder="# Hello" :value="rawText" @input="update" :rows="rows"></textarea>
-    <div v-html="markedText"></div>
-    <div id="menu">
+    <div id="menu" class="hogeMenu" style="display: flex; background-color: #2d4e6f; padding: 15px;">
       <button v-on:click="save()">SAVE</button>
       <button v-on:click="load()">LOAD</button>
+    </div>
+    <div class="wrap-editArea">
+      <textarea class="edit-mdArea" placeholder="# Hello" :value="rawText" @input="update" :rows="rows"></textarea>
+      <div class="view-text" v-html="markedText"></div>
     </div>
   </div>
 </template>
@@ -50,27 +52,33 @@ export default {
 </script>
 
 <style>
-html, body, #app {
+html, body {
   margin: 0;
-  height: 95vh;
+  height: 100%;
   font-family: 'Helvetica Neue', Arial, sans-serif;
   color: #333;
 }
 
-div#menu {
-    display: flex;
+#app {
+  height: 100vh;
+  overflow: hidden;
 }
-
-textarea, #app div {
-  display: inline-block;
-  width: 49%;
-  height: 95vh;
-  vertical-align: top;
-  box-sizing: border-box;
-  padding: 0 20px;
+ 
+ 
+.hogeMenu {
+  display: flex;
+  background-color: #000;
+￼  padding: 15px;
+  display: flex !important;
+  background-color: #2d4e6f !important;
+  padding: 15px !important;
 }
-
-textarea {
+.wrap-editArea {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+.edit-mdArea {
   border: none;
   border-right: 1px solid #ccc;
   resize: none;
@@ -79,7 +87,33 @@ textarea {
   font-size: 14px;
   font-family: 'Monaco', courier, monospace;
   padding: 20px;
+  width: 50%;
+  height: 100%;
 }
+.view-text {
+  padding:20px;
+  box-sizing: border-box;
+}
+
+/*textarea, #app div {
+  display: inline-block;
+  width: 49%;
+  height: 100%;
+  vertical-align: top;
+  box-sizing: border-box;
+  padding: 0 20px;
+}*/
+
+/*textarea {
+  border: none;
+  border-right: 1px solid #ccc;
+  resize: none;
+  outline: none;
+  background-color: #f4f9ff;
+  font-size: 14px;
+  font-family: 'Monaco', courier, monospace;
+  padding: 20px;
+}*/
 
 code {
   color: #f66;
